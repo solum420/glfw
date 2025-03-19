9dbef78df6e9bc74bd0d3a9b63e5ef8dfd5ad367
solum420 <88638227+solum420@users.noreply.github.com>
2025-03-19 17:23:13 -0400

    aaaaaaaaa

--- premake5.lua
+++ premake5.lua
@@ -1,60 +1,58 @@
 project "GLFW"
-	kind "StaticLib"
-	language "C"
-	staticruntime "off"
-	warnings "off"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
     toolset "clang"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+    files {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
 
-	files
-	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
+    files {
+        "src/win32_init.c",
+        "src/win32_joystick.c",
+        "src/win32_module.c",
+        "src/win32_monitor.c",
+        "src/win32_time.c",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
+    }
 
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
+    defines {
+        "_GLFW_WIN32",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
 
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
-	}
+    filter "system:windows"
+        systemversion "latest"
 
-	files
-	{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_module.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
-	}
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
-	systemversion "latest"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "speed"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "speed"
-		symbols "off"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "Speed"
+        
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "Speed"
